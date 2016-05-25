559bdfa647d529c746cf12878d545045cacf32d2
dsdshcym <dsdshcym@gmail.com>
2016-05-25 11:04:23 +0800

    Add toggle_item method to TodoList

--- todolist.rb
+++ todolist.rb
@@ -15,6 +15,10 @@ class TodoList
     @items.delete_at(item_id - 1)
   end
 
+  def toggle_item(item_id)
+    @items[item_id - 1].toggle
+  end
+
   def rename(new_title)
     @title = new_title
   end
@@ -26,5 +30,9 @@ class Item
   def initialize(item_description)
     @description = item_description
     @completed_status = false
+  end
+
+  def toggle
+    @completed_status = !@completed_status
   end
 end
