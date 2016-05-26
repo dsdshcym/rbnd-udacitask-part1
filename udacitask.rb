f979c0431fe029abf3eef686dd2c3dd4febe386f
dsdshcym <dsdshcym@gmail.com>
2016-05-26 09:06:13 +0800

    Complete TODOes in udacitask.rb

--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,36 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
+my_list = TodoList.new("My Todo List")
 
 # Add four new items
+%w(First Second Third Fourth).each do |title|
+  my_list.add_item(title)
+end
 
 # Print the list
+my_list.print
 
 # Delete the first item
+my_list.delete_item(1)
 
 # Print the list
+my_list.print
 
 # Delete the second item
+my_list.delete_item(2)
 
 # Print the list
+my_list.print
 
 # Update the completion status of the first item to complete
+my_list.toggle_item(1)
 
 # Print the list
+my_list.print
 
 # Update the title of the list
+my_list.rename("My NEW Todo List")
 
 # Print the list
+my_list.print
